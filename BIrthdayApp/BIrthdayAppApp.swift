8201e63f46f56b75f889cc8d670c8bba979a4ed9
Keely Hong <keelyhong06@gmail.com>
2025-07-11 10:12:07 -0400

    Initial Commit

---
+++ BIrthdayApp/BIrthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIrthdayAppApp.swift
+//  BIrthdayApp
+//
+//  Created by Keely Hong on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BIrthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BIrthdayApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BIrthdayApp
+//
+//  Created by Keely Hong on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
