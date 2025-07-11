c0da0a41bf93a6eb5c81e799f6f69490be45ff36
Keely Hong <keelyhong06@gmail.com>
2025-07-11 12:08:31 -0400

    Create Swift UI

--- BIrthdayApp/ContentView.swift
+++ BIrthdayApp/ContentView.swift
@@ -1,24 +1,64 @@
 //
 //  ContentView.swift
-//  BIrthdayApp
+//  Birthday-app
 //
 //  Created by Keely Hong on 7/11/25.
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    //friends array
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List (friends){
+                friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+        .navigationTitle("Birthdays")
+        .safeAreaInset(edge: .bottom){
+            VStack (alignment: .center, spacing: 20){
+                Text("New Birthday")
+                    .font (.headline)
+                DatePicker(selection: $newBirthday, in:
+                            Date.distantPast...Date.now,
+                           displayedComponents: .date){
+                    TextField("Name", text:
+                                $newName)
+                    .textFieldStyle(
+                        .roundedBorder)
+                }
+                Button("Save"){
+                    let newFriend = Friend(name: newName,
+                                           birthday:
+                                            newBirthday)
+                        context.insert(newFriend)
+                    newName = ""
+                    newBirthday = .now
+                }
+                .bold()
+            }
         }
-        .padding()
+        }
+        //closing nav stack
     }
+    //closing the body
 }
+//closing the struct
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self,
+                        inMemory: true)
 }
